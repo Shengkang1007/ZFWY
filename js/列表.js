67f3f6b62985bf3e9f2928982c23db6ceaf6e61f
ztt19950808 <463649169@qq.com>
2021-03-11 14:23:41 +0800

    xxxx

--- js/列表.js
+++ js/列表.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-09 11:29:58
+ * @LastEditTime: 2021-03-11 11:49:21
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \gshop-server_finale:\有关JS、vue的练习\移动端\找房无忧\ZFWY\js\列表.js
+ */
 
 $('.cc').click(function(){
     $('.b_').toggle();
@@ -45,4 +53,31 @@ $('.cc3').click(function(){
 $('.b_2>li>span').on('click',function(){
     $(this).addClass('cc_');
     $(this).parent().siblings().find('span').removeClass('cc_');
+   
 });
+
+$('.b_3 #uls li').click(function(){
+    $(this).css('color','red').siblings().css('color','').removeClass('nan')
+})
+$('.b_3 #uls1 li').click(function(){
+    $(this).css('color','red').siblings().css('color','').removeClass('wushi')
+ })
+ $('.b_3 #uls2 li').click(function(){
+    $(this).css('color','red').siblings().css('color','').removeClass('xuequ')
+ })
+
+ $('.b_2 #lis').click(function(){
+     $('.b_2').css('display','none')
+ })
+
+ $('.b_1 li').click(function(){
+    $(this).css('color','red').siblings().css('color','').removeClass('buxian')
+ })
+
+ $('.b_ #uls3 li').click(function(){
+     $(this).css('color','#ff8a00').siblings().css('color','').removeClass('lei')
+ })
+
+ $('.b_ #uls4 li').click(function(){
+    $(this).css('color','#ff8a00').siblings().css('color','').removeClass('ming')
+})
